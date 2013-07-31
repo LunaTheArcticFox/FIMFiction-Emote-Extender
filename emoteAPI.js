e936105dea400c0df758eb6d4ad0bc9c21085f3a
KrazyTheFox <traveler@krazyweb.net>
2013-07-31 12:41:16 -0400

    Initial commit

---
+++ emoteAPI.js
@@ -0,0 +1,12 @@
+/*
+ *	Fimfiction Emote Script v2.0
+ *
+ *	Written by KrazyTheFox
+ */
+
+window.initialized = false;
+window.emoteTables = [];
+
+function initialize() {
+	console.log("Success");
+}
---
+++ standardTables.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Fimfiction Emote Extender
+// @namespace ffemoteextender
+// @description Adds additional emotes to fimfiction.net.
+// @include http*://www.fimfiction.net/*
+// @include http*://fimfiction.net/*
+// @grant none
+// @require https://github.com/iloveportalz0r/fimfiction-script/raw/master/emoteHandler.js
+// @version 2.0
+// ==/UserScript==
+

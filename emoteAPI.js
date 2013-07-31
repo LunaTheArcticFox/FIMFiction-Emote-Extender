36e1b3ba401a0cc104d973940eb045b90cc53479
KrazyTheFox <traveler@krazyweb.net>
2013-07-31 14:58:10 -0400

    Added page identification, logging, and some CSS.

--- emoteAPI.js
+++ emoteAPI.js
@@ -7,31 +7,86 @@
 window.initialized = false;
 window.emoteTables = [];
 
+var sitePage = PAGE.OTHER;
+ 
+var Page = {
+	OTHER:	{value: -1, name: "Other"},
+	BLOGEDIT:	{value: 0, name: "Blog Editor"},
+	GROUPTHREAD:	{value: 1, name: "Group Thread"},
+};
+
+function logInfo(message) {
+	console.log("Emote API [INFO]: " + message);
+}
+
+function logError(message) {
+	console.log("Emote API [ERROR]: " + message);
+}
+
+window.onerror = function(message, url, line) {
+	logError(message + " at line: " + line);
+}
+
+// GM function replacements are from https://raw.github.com/gist/3123124
+function GM_addStyle(aCss) {
+	
+	var head = document.getElementsByTagName("head")[0];
+	
+	if(head) {
+		var stylenode = document.createElement("style");
+		stylenode.type = "text/css";
+		stylenode.textContent = aCss;
+		head.appendChild(stylenode);
+		return stylenode;
+	}
+	
+	return null;
+	
+}
+
 function initialize() {
 	
 	loadScript('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', function() {
-		init();
+		setupPage();
 	});
 	
 }
 
-function init() {
+function setupPage() {
+	
+	getSitePage();
 	
 	if (window.initialized) {
 		return;
 	}
 	
-	$('div.textbox_container').css('height', '600');
-	$('textarea#comment_comment').css('height', '100% !important');
-	$('div.addcomment').css('background-color', '#000000');
-	
 	initialized = true;
 	
-	console.log("Success");
+	var theCSS = [];
+	
+	theCSS.push(
+		
+		".emoticons_panel {",
+			"height: auto !important;",
+			"min-height: 300px !important;",
+			"display: block !important;",
+		"}"
+		
+		
+		
+	);
+	
+	GM_addStyle(theHTML.join(''));
+	
+	logInfo("Initialized successfully.");
 	
 }
 
 function addEmote(url, emoteName, shortTableName, longTableName) {
+	
+	if (!window.initialized) {
+		initialize();
+	}
 	
 }
 
@@ -46,5 +101,19 @@ function loadScript(url, callback) {
 	script.onload = callback;
 	
 	head.appendChild(script);
+	
+}
+
+function getSitePage() {
+	
+	if(/\/manage_user\/edit_blog_post/.test(location.href)) {
+		sitePage = Page.BLOGEDIT;
+		logInfo("Site page set to blog editor.");
+	} else if(/\/group\//.test(location.href)) {
+		if (/\/thread\//.test(location.href)) {
+			sitePage = PAGE.GROUPTHREAD;
+			logInfo("Site page set to group thread.");
+		}
+	}
 	
 }

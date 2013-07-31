2bed957af67f382af7eb958e5c63e31fed33c9ea
KrazyTheFox <traveler@krazyweb.net>
2013-07-31 13:11:33 -0400

    Testing CSS functionality.

--- emoteAPI.js
+++ emoteAPI.js
@@ -8,5 +8,15 @@ window.initialized = false;
 window.emoteTables = [];
 
 function initialize() {
+	
+	initialized = true;
+	
+	$(".textbox_container").css("height", "600");
+	$("#comment_comment").css("height", "100%");
+	
 	console.log("Success");
+}
+
+function addEmote(url, emoteName, shortTableName, longTableName) {
+	
 }
--- standardTables.user.js
+++ standardTables.user.js
@@ -6,7 +6,14 @@
 // @include http*://fimfiction.net/*
 // @grant none
 // @require https://bitbucket.org/KrazyTheFox/fimfiction-emote-extender/raw/e936105dea400c0df758eb6d4ad0bc9c21085f3a/emoteAPI.js
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @version 2.0
 // ==/UserScript==
 
-initialize();
+$(document).ready( function() {
+	
+	initialize();
+	
+	
+	
+});

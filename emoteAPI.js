206fb6bbd46f11e31e3a513b0546692ea918c737
KrazyTheFox <traveler@krazyweb.net>
2013-08-01 01:04:28 -0400

    Yet another test.

--- emoteAPI.js
+++ emoteAPI.js
@@ -4,11 +4,6 @@
  *	Written by KrazyTheFox
  */
 
-top.initialized = false;
-top.emoteTables = [];
-top.tabContainer;
-top.tablePrefix = "emoteAPI_Table:";
-
 var pageOther = -1;
 var pageGroupThread = 0;
 var pageBlogEdit = 1;
@@ -46,6 +41,13 @@ function addGlobalStyle(css) {
 function initialize() {
 	
 	logInfo("Initializing...");
+
+	if (initialized) {
+		logError("Already initialized, returning.");
+		return;
+	}
+
+	addScript("var initialized = true; var emoteTables = []; var tabContainer; var tablePrefix = 'emoteAPI_Table:';");
 	
 	getSitePage();
 
@@ -72,8 +74,8 @@ function initialize() {
 
 	}
 	
-	if (top.initialized || sitePage == pageOther) {
-		logError("Already initialized, returning.");
+	if (sitePage == pageOther) {
+		logError("Page not compatible, returning.");
 		return;
 	}
 	
@@ -448,4 +450,23 @@ function GM_listValues() {
 function GM_setValue(aKey, aVal) {
   'use strict';
   localStorage.setItem(__GM_STORAGE_PREFIX + aKey, aVal);
+}
+
+function addScript(source) {
+  // Check for function input.
+  if ('function' == typeof source) {
+    // Execute this function with no arguments, by adding parentheses.
+    // One set around the function, required for valid syntax, and a
+    // second empty set calls the surrounded function.
+    source = '(' + source + ')();'
+  }
+
+  // Create a script node holding this  source code.
+  var script = document.createElement('script');
+  script.setAttribute("type", "application/javascript");
+  script.textContent = source;
+
+  // Insert the script node into the page, so it will run, and immediately
+  // remove it to clean up.
+  document.body.appendChild(script);
 }

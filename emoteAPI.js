8505a99f0accf5dfdfaac035511b4b424c2dbf52
KrazyTheFox <traveler@krazyweb.net>
2013-07-31 22:19:29 -0400

    FFS.

--- emoteAPI.js
+++ emoteAPI.js
@@ -285,42 +285,45 @@ function replaceSelectedText(el, text) {
 
 function getInputSelection(el) {
 
-	var _start = 0, _end = 0, normalizedValue, range, textInputRange, len, endRange;
+    var start = 0, end = 0, normalizedValue, range,
+        textInputRange, len, endRange;
 
-	if(typeof el.selectionStart === "number" && typeof el.selectionEnd === "number") {
-		_start = el.selectionStart;
-		_end = el.selectionEnd;
-	} else {
+    if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+        start = el.selectionStart;
+        end = el.selectionEnd;
+    } else {
+        range = document.selection.createRange();
 
-		range = window.getSelection();
+        if (range && range.parentElement() == el) {
+            len = el.value.length;
+            normalizedValue = el.value.replace(/\r\n/g, "\n");
 
-		if(range && range.parentElement() == el) {
+            // Create a working TextRange that lives only in the input
+            textInputRange = el.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
 
-			len = el.value.length;
-			normalizedValue = el.value.replace(/\r\n/g, "\n");
+            // Check if the start and end of the selection are at the very end
+            // of the input, since moveStart/moveEnd doesn't return what we want
+            // in those cases
+            endRange = el.createTextRange();
+            endRange.collapse(false);
 
-			textInputRange = el.createTextRange();
-			textInputRange.moveToBookmark(range.getBookmark());
+            if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                start = end = len;
+            } else {
+                start = -textInputRange.moveStart("character", -len);
+                start += normalizedValue.slice(0, start).split("\n").length - 1;
 
-			endRange = el.createTextRange();
-			endRange.collapse(false);
-
-			if(textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
-				_start = _end = len;
-			} else {
-				_start = -textInputRange.moveStart("character", -len);
-				_start += normalizedValue.slice(0, _start).split("\n").length - 1;
-
-				if(textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
-					_end = len;
-				} else {
-					_end = -textInputRange.moveEnd("character", -len);
-					_end += normalizedValue.slice(0, _end).split("\n").length - 1;
-				}
-			}
-		}
-	}
-
+                if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                    end = len;
+                } else {
+                    end = -textInputRange.moveEnd("character", -len);
+                    end += normalizedValue.slice(0, end).split("\n").length - 1;
+                }
+            }
+        }
+    }
+    
 	return {
 	start: _start,
 	end: _end

ac1309fdf44fd9c0ca59eafe52e285ef8c5bde32
KrazyTheFox <traveler@krazyweb.net>
2013-08-01 00:47:18 -0400

    Added all emotes to the tables. Created Cheerilee module.

---
+++ cheerileeTable.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name Fimfiction Emote Extender: Cheerilee Module
+// @namespace ffemoteextendercheerilee
+// @description Adds additional emotes to fimfiction.net.
+// @include http*://www.fimfiction.net/*
+// @include http*://fimfiction.net/*
+// @grant none
+// @require https://raw.github.com/KrazyTheFox/Fimfiction-Emote-API/master/emoteAPI.js
+// @version 2.0
+// ==/UserScript==
+
+$(document).ready(run());
+
+function run() {
+
+	addEmote("http://i.imgur.com/IdiNEoG.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/zi55aHB.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/Z9dmrsL.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/etvChCJ.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/4CT6FFE.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/sqalbWn.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/rRAQgvt.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/8uzdXWe.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/PsTsg73.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/7OlsrI3.png", "Emote", "CHR", "Cheerilee");
+	addEmote("http://i.imgur.com/PC1vZ9r.png", "Emote", "CHR", "Cheerilee");
+
+}

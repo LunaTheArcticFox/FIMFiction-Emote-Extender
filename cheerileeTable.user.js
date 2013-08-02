c473ce66cfec23153aa88279185e12bc8e477b51
KrazyTheFox <traveler@krazyweb.net>
2013-08-01 22:36:15 -0400

    Abort, abort!

--- cheerileeTable.user.js
+++ cheerileeTable.user.js
@@ -4,6 +4,7 @@
 // @description Adds additional emotes to fimfiction.net.
 // @include http*://www.fimfiction.net/*
 // @include http*://fimfiction.net/*
+// @grant none
 // @require https://raw.github.com/KrazyTheFox/Fimfiction-Emote-API/master/emoteAPI.js
 // @version 2.0b
 // ==/UserScript==

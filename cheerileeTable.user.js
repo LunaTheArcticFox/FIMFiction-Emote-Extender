731da9818f75d0f86441c068549035cd111e869c
KrazyTheFox <traveler@krazyweb.net>
2013-08-20 23:39:29 -0400

    Changed emote sorting to Jondor's tabs.

--- cheerileeTable.user.js
+++ cheerileeTable.user.js
@@ -13,16 +13,17 @@ $(document).ready(run());
 
 function run() {
 
-	addEmote("http://i.imgur.com/IdiNEoG.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/zi55aHB.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/Z9dmrsL.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/etvChCJ.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/4CT6FFE.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/sqalbWn.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/rRAQgvt.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/8uzdXWe.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/PsTsg73.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/7OlsrI3.png", "Emote", "CHR", "Cheerilee");
-	addEmote("http://i.imgur.com/PC1vZ9r.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/cvM4P.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/IdiNEoG.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/zi55aHB.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/Z9dmrsL.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/etvChCJ.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/4CT6FFE.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/sqalbWn.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/rRAQgvt.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/8uzdXWe.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/PsTsg73.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/7OlsrI3.png", "Emote", "CHR", "Cheerilee");
+    addEmote("http://i.imgur.com/PC1vZ9r.png", "Emote", "CHR", "Cheerilee");
 
 }

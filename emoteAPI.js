c873bf6d7d9f8c9e3afc9a4aaf99e789194effd2
KrazyTheFox <traveler@krazyweb.net>
2013-10-23 02:53:52 -0400

    q

--- emoteAPI.js
+++ emoteAPI.js
@@ -85,7 +85,7 @@ function initialize() {
 		$(".light_toolbar").after(getDefaultTableHTML());
 		$("#blog_post_content").parent().css("margin-right", "300px");
 		$(".emoticons_panel").after("<p style='display: block;'>Blog post support for the emote script is undergoing development. While it should be fully functional, the styling is actively being worked on and looks this way intentionally. Kind of.</p>");
-		$(".add_comment form").submit(function(e) {
+		$(".add_comment form").bind("submit", function(e) {
 			alert("Hey-o!");
 			parseEmotesInForm(e);
 		});

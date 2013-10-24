78dc69192623bc6b5142c13e5b6388ad4f7a38c2
KrazyTheFox <traveler@krazyweb.net>
2013-10-24 13:13:19 -0400

    Updated versioning for development branch.

--- standardTables.user.js
+++ standardTables.user.js
@@ -6,7 +6,7 @@
 // @include http*://fimfiction.net/*
 // @grant none
 // @require https://raw.github.com/KrazyTheFox/Fimfiction-Emote-API/master/emoteAPI.js
-// @version 2.0
+// @version 3.0d
 // ==/UserScript==
 
 $(document).ready(run());

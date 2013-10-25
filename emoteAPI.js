757c4767a7bd57fc4f04846d9078124561692835
KrazyTheFox <traveler@krazyweb.net>
2013-10-24 21:04:05 -0400

    Test

--- emoteAPI.js
+++ emoteAPI.js
@@ -96,11 +96,6 @@ function initialize() {
 		});
 	});
 
-	$(".data form").each(function(index) {
-		//$(this).removeAttr("onsubmit");
-		$(this).attr("onsubmit", "parseCommentEdit(this); return false;");
-	});
-
 	if (sitePage == pageOther) {
 		return;
 	}
@@ -768,22 +763,31 @@ function parseCommentSubmission() {
 
 }
 
-function parseCommentEdit(form) {
 
-	alert("Hey!");
-
-	var tempForm = $(form);
+EditComment = function(form, comment) {
 
 	$(".textarea_padding textarea").each(function(index) {
 		$(this).html(emoteShorthandToImages($(this).html()));
 	});
 
-	var commentID = tempForm.attr("id");
-	commentID = "#" + commentID.split("_")[3];
-
-	EditComment(form, $(commentID));
-
-	return false;
+	$(comment).find(".edit_area").find(".save_button").find("img").attr("src", "http://static.fimfiction.net/images/loading_white.gif");
+	$(comment).find(".edit_area").find(".save_button").find("img").attr("width", "16");
+	$.post('/ajax/edit_comment.php',
+		$(form).serialize( ), 
+		function(xml) 
+		{
+            if ( $("error",xml).length )
+            {
+				ShowErrorWindow( $("error",xml).text( ) );
+            }
+            else
+            {
+				$(comment).find( ".comment_data" ).html( $("content",xml).text( ) );
+				$(comment).find(".edit_area").find(".save_button").find("img").attr("src", "http://static.fimfiction.net/images/save.png");
+				ToggleEditComment( comment );
+            }
+		} 
+	);	
 
 }
 
